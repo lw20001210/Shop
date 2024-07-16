973a7994ec2276e2d171fa8efc663054db8e563e
linwei <135044867+lw20001210@users.noreply.github.com>
2024-07-16 23:22:49 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <Header></Header>
+    <nav><el-button>Default</el-button>
+      <el-button type="primary">Primary</el-button>
+      <el-button type="success">Success</el-button>
+    </nav>
+    <div class="queryTotalGood">查看所有成品分类</div>
+    <main>
+      主体内容
+    </main>
+    <footer>
+      底部
+    </footer>
+  </div>
+</template>
+<script setup lang="ts">
+import Header from "./components/common/Header.vue"
+</script>
+<style scoped lang="less">
+.container {
+  margin: 0 auto;
+
+  nav {
+    background: skyblue;
+    height: 300px;
+  }
+
+  .queryTotalGood {
+    width: 90%;
+    background-color: rgb(26, 248, 122);
+    height: 30px;
+    line-height: 30px;
+    text-align: center;
+    margin: 10px auto 50px;
+  }
+}
+</style>
---
+++ src/components/common/Header.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="header">
+    <div class="left">
+      <img class="logoImg" :src="logoPng" alt="" />
+    </div>
+    <div class="center">中</div>
+    <div class="right">
+      <!-- <el-input
+        placeholder="请输入内容"
+        v-model="inputContent"
+        class="input-with-select"
+      >
+      </el-input> -->
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import logoPng from "../../assets/logos/logo.jpg";
+</script>
+
+<style lang="less" scoped>
+.header {
+  position: sticky;
+  top: 0;
+  color: red;
+  height: 90px;
+  background-color: pink;
+  display: flex;
+  padding: px 10%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  .left,
+  .right {
+    flex: 1;
+  }
+
+  .left {
+    text-align: right;
+
+    .logoImg {
+      object-fit: cover;
+      width: 70px;
+      border-radius: 50%;
+    }
+  }
+
+  .center {
+    flex: 8;
+  }
+}
+</style>
