d78f25624cb482a6c7c392491a837e8d502dd1ae
linwei <135044867+lw20001210@users.noreply.github.com>
2024-07-24 17:12:45 +0800

    feat:切换路由添加loading效果

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import 'element-plus/theme-chalk/el-loading.css'
 import {router} from "@/router/index"
 const app = createApp(App)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
