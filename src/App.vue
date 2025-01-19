ef42bd69a2e51416f81face8ae4844864f600327
linwei <2972063787@qq.com>
2025-01-19 18:33:10 +0800

    test:测试自动触发打包部署dockerfile

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="container">
-    <!-- <Header></Header> -->
+     <Header></Header>
     <RouterView></RouterView>
-    <Footer></Footer>
+<!--    <Footer></Footer>-->
   </div>
 </template>
 <script setup lang="ts">
